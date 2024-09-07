2f4836c257e7b4dacebae3a998f11a9b1cadecfc
noahdossan <146362500+noahdossan@users.noreply.github.com>
2024-09-06 21:11:37 -0700

    Test update 3

--- SetupLinux.sh
+++ SetupLinux.sh
@@ -1,28 +1,48 @@
 #!/bin/bash
 
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+
 echo "Installing packages. Please wait..."
 echo "Installing for Linux..."
 echo "2"
 
-cd
-
+# Update and upgrade the system
 sudo apt-get update
 sudo apt-get upgrade -y
 
 # Install git
-sudo apt-get install -y git 
-echo "Installed git."
+echo -e $b">"$w" installing modules: "$g"git"$w
+sudo apt-get install -y git
 
-# Install Python 3 and pip for Python 3
-sudo apt-get install -y python3 python3-pip
-echo "Installed Python 3 and pip."
+# Install Python 3, venv, and pipx
+echo -e $b">"$w" installing modules: "$g"python3 and venv"$w
+sudo apt-get install -y python3 python3-venv python3-pip pipx
 
-# Install required Python packages
-pip3 install requests
-pip3 install bs4
+# Create a virtual environment
+echo -e $b">"$w" creating a virtual environment"
+python3 -m venv myenv
+
+# Activate the virtual environment
+echo -e $b">"$w" activating the virtual environment"
+source myenv/bin/activate
+
+# Install necessary Python packages within the virtual environment
+echo -e $b">"$w" installing modules: "$g"requests and bs4"$w
+pip install requests bs4
+
+# Navigate to the Free-Proxy directory and run the scripts
+chmod +x Free-Proxy
+cd ~/Free-Proxy/src
+bash Free-Proxy.sh
+python3 menu.py
+
+# Deactivate the virtual environment after execution
+deactivate
 
 echo "Setup for Linux successful."
-
 echo "██████╗░░█████╗░███╗░░██╗███████╗"
 echo "██╔══██╗██╔══██╗████╗░██║██╔════╝"
 echo "██║░░██║██║░░██║██╔██╗██║█████╗░░"
